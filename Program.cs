041d0bbf3b2957dad3e6100287ff4b12fed31655
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-12-04 20:53:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+           Console.Clear();
+           Console.WriteLine("Digite o primeiro valor: ");
+           float valor1 = float.Parse(Console.ReadLine()); 
+        }
+    }
+}
