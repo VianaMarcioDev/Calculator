e2dfeb1251980d94952f26e41825ec0b2e8b8265
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-12-04 21:03:02 -0300

    Criar entra de dois números e saída da soma deles.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,19 @@ namespace Calculator
 {
     class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
            Console.Clear();
            Console.WriteLine("Digite o primeiro valor: ");
-           float valor1 = float.Parse(Console.ReadLine()); 
+           float primeiroValor = float.Parse(Console.ReadLine()); 
+           Console.WriteLine("Digite o segundo valor: ");
+           float segundoValor = float.Parse(Console.ReadLine());
+
+           float soma = primeiroValor + segundoValor;
+
+           Console.WriteLine();
+           Console.WriteLine($"O resultado da soma entre {primeiroValor} e {segundoValor} é {soma}.");
+           
         }
     }
 }
