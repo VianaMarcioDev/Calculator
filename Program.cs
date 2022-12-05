b4921438a23f99fb5997fa9f1d6329e229b87cd7
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-12-04 21:10:30 -0300

    Implementando método de subtração

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-           Soma();
+           Subtracao();
            
         }
 
         static void Soma()
         {
-            Console.Clear();
+           Console.Clear();
            Console.WriteLine("Digite o primeiro valor: ");
            float primeiroValor = float.Parse(Console.ReadLine()); 
            Console.WriteLine("Digite o segundo valor: ");
@@ -22,6 +22,20 @@ namespace Calculator
 
            Console.WriteLine();
            Console.WriteLine($"O resultado da soma entre {primeiroValor} e {segundoValor} é {soma}.");
+        }
+
+        static void Subtracao()
+        {
+           Console.Clear();
+           Console.WriteLine("Digite o primeiro valor: ");
+           float primeiroValor = float.Parse(Console.ReadLine()); 
+           Console.WriteLine("Digite o segundo valor: ");
+           float segundoValor = float.Parse(Console.ReadLine());
+
+           float subtracao = primeiroValor - segundoValor;
+
+           Console.WriteLine();
+           Console.WriteLine($"O resultado da subtração entre {primeiroValor} e {segundoValor} é {subtracao}.");
         }
     }
 }
