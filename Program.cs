eeb06cd0d46cdc7e75470cc7ad1cee713e09cd9e
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-12-04 21:07:34 -0300

    Refatorando código de soma para função própria

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-           Console.Clear();
+           Soma();
+           
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
            Console.WriteLine("Digite o primeiro valor: ");
            float primeiroValor = float.Parse(Console.ReadLine()); 
            Console.WriteLine("Digite o segundo valor: ");
@@ -16,7 +22,6 @@ namespace Calculator
 
            Console.WriteLine();
            Console.WriteLine($"O resultado da soma entre {primeiroValor} e {segundoValor} é {soma}.");
-           
         }
     }
 }
